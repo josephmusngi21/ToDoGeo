b68c2e454e028b21886715151750728b03b0553b
Josephmusngi21 <josephmusngi7@gmail.com>
2025-02-01 02:21:12 -0700

    moved User to the top of screen

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,18 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 import User from "../components/User/User";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <User />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <User/>
+            <View style={styles.user}></View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        paddingTop: 0,
+    },
+    user: {
+        width: "100%",
+        alignItems: "center",
+    },
+});
